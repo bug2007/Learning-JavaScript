d1049eeac14e69ed31eff5e70eb900a5995045a5
bug2007 <moumitah2007@gmail.com>
2026-04-11 22:04:13 +0600

    Functions

---
+++ sec1-basics/index.js
@@ -0,0 +1,11 @@
+function greet(name, lastName) { // parameter
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('John', 'Smith'); // arguments
+
+function square(number) {
+    return number * number; 
+}
+
+console.log(square(2));
