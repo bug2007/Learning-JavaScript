a108191293dcdc6b71d78dc518bb8fdf5fa4812b
bug2007 <moumitah2007@gmail.com>
2026-04-13 20:03:38 +0600

    functions are objects

---
+++ sec5-objects/6-funcs-are-objs/index.js
@@ -0,0 +1,14 @@
+// in js, functions are objects
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+Circle.call({}, 1);
+Circle.apply({}, [1, 2, 3]);
+
+
+const another = new Circle(1);
